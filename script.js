3fe61714e726779c6d356d5403794a3d9a2f7909
manavnagpal08 <72203264+manavnagpal08@users.noreply.github.com>
2025-07-04 12:04:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-// script.js
-
 // Initialize AOS animations
 AOS.init({
   duration: 1200,
   once: true
 });
+
+// Toggle mobile menu
+function toggleMenu() {
+  const nav = document.getElementById('navLinks');
+  nav.classList.toggle('open');
+}
